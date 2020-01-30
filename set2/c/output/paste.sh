107f440c9af6179c38e0fc7428be6c3d6d45eb0b
George Z. Zachos <gzachos@cse.uoi.gr>
2020-01-30 08:42:00 +0200

    Removed output/

--- set2/c/output/paste.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-paste -d " "  x1_100_sd.txt x1_100_cg.txt > x1_100.txt
-paste -d " "  x1_1000_sd.txt x1_1000_cg.txt > x1_1000.txt
-paste -d " "  x1_10000_sd.txt x1_10000_cg.txt > x1_10000.txt
-paste -d " "  x2_100_sd.txt x2_100_cg.txt > x2_100.txt
-paste -d " "  x2_1000_sd.txt x2_1000_cg.txt > x2_1000.txt
-paste -d " "  x2_10000_sd.txt x2_10000_cg.txt > x2_10000.txt
